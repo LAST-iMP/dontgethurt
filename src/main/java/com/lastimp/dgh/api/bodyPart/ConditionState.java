2ab8ddffb9c0b877391f4cd9ed7fd5d1961c4787
LAST-iMP <xiaoshibang@126.com>
2025-11-24 12:01:07 +0800

    init version 1.1.0.2

--- src/main/java/com/lastimp/dgh/api/bodyPart/ConditionState.java
+++ src/main/java/com/lastimp/dgh/api/bodyPart/ConditionState.java
@@ -84,6 +84,10 @@ public class ConditionState implements INBTSerializable<CompoundTag> {
         return hiddenValue;
     }
 
+    public float getTotalValue() {
+        return hiddenValue + value;
+    }
+
     public void setHiddenValue(float hiddenValue) {
         this.hiddenValue = hiddenValue;
     }
