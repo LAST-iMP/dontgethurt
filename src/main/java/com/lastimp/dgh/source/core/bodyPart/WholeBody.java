2b08ccf0e22fd53d60814779f0327fa0bac9361e
LAST-iMP <xiaoshibang@126.com>
2025-11-25 00:43:50 +0800

    init version 1.1.0.7

--- src/main/java/com/lastimp/dgh/source/core/bodyPart/WholeBody.java
+++ src/main/java/com/lastimp/dgh/source/core/bodyPart/WholeBody.java
@@ -1,29 +1,3 @@
-/*
-* MIT License
-
-Copyright (c) 2023 NeoForged project
-
-This license applies to the template files as supplied by github.com/NeoForged/MDK
-
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-*/
 
 package com.lastimp.dgh.source.core.bodyPart;
 
@@ -33,12 +7,14 @@ import com.lastimp.dgh.api.enums.BodyComponents;
 import com.lastimp.dgh.api.enums.BodyCondition;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.util.Mth;
 import net.minecraft.world.entity.player.Player;
 
 import java.util.HashMap;
 import java.util.List;
 
 import static com.lastimp.dgh.api.enums.BodyComponents.*;
+import static com.lastimp.dgh.api.enums.BodyCondition.*;
 
 public class WholeBody extends AbstractBody {
     private final HashMap<BodyComponents, AbstractBody> components = new HashMap<>();
@@ -90,6 +66,15 @@ public class WholeBody extends AbstractBody {
             lost += this.getComponent(components).updateVitalityLost(health, player);
         }
         return lost;
+    }
+
+    @Override
+    public int slowDownLevel() {
+        int slowDown = 0;
+        for (var body : this.components.values()) {
+            slowDown += body.slowDownLevel();
+        }
+        return Mth.clamp(slowDown, 0, 10);
     }
 
     @Override
