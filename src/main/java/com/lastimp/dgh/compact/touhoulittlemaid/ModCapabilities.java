feb32de86beeef28b92abadd1f93330c2cfd3066
LAST-iMP <xiaoshibang@126.com>
2025-12-15 22:45:55 +0800

    init version 1.1.7

---
+++ src/main/java/com/lastimp/dgh/compact/touhoulittlemaid/ModCapabilities.java
@@ -0,0 +1,23 @@
+package com.lastimp.dgh.compact.touhoulittlemaid;
+
+import com.github.tartaricacid.touhoulittlemaid.entity.passive.EntityMaid;
+import com.lastimp.dgh.DontGetHurt;
+import com.lastimp.dgh.source.core.capability.HealthProvider;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.Entity;
+import net.minecraftforge.event.AttachCapabilitiesEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModList;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = DontGetHurt.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
+public class ModCapabilities {
+    @SubscribeEvent
+    public static void onAttachCapabilities(AttachCapabilitiesEvent<Entity> event) {
+        if (!ModList.get().isLoaded("touhou_little_maid")) return;
+        if (event.getObject() instanceof EntityMaid) {
+            event.addCapability(ResourceLocation.fromNamespaceAndPath(DontGetHurt.MODID, "health"), new HealthProvider());
+        }
+        HealthProvider.add(EntityMaid.class);
+    }
+}
