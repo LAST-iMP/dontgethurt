b762f103c0249596eebee3e0d3203ee9b683bb3e
LAST-iMP <xiaoshibang@126.com>
2025-12-19 02:22:59 +0800

    init version 1.1.8

--- src/main/java/com/lastimp/dgh/source/core/buffSystem/BuffEventHandler.java
+++ src/main/java/com/lastimp/dgh/source/core/buffSystem/BuffEventHandler.java
@@ -38,13 +38,12 @@ public class BuffEventHandler {
     }
 
     private static void updateStaggerEffects(HealthCapability health, LivingEntity entity) {
-        if (health.slowDown() > 0 && !entity.hasEffect(ModEffects.STAGGER_EFFECT)) {
-            var newEffect = new MobEffectInstance(
-                    ModEffects.STAGGER_EFFECT,
-                    40, health.slowDown() - 1
-            );
-            entity.addEffect(newEffect);
-        }
+        if (health.slowDown() <= 0) return;
+        var newEffect = new MobEffectInstance(
+                ModEffects.STAGGER_EFFECT,
+                40, health.slowDown() - 1
+        );
+        entity.addEffect(newEffect);
     }
 
     private static void updateArmEffects(HealthCapability health, LivingEntity entity) {
@@ -54,30 +53,25 @@ public class BuffEventHandler {
                 MobEffects.DIG_SLOWDOWN,
                 40, health.armBreak() - 1
         );
-        if (!entity.hasEffect(MobEffects.DIG_SLOWDOWN)) {
-            entity.addEffect(newEffect);
-        } else if (entity.getEffect(MobEffects.DIG_SLOWDOWN).getAmplifier() >= health.armBreak()) {
-            entity.getEffect(MobEffects.DIG_SLOWDOWN).update(newEffect);
-        }
+        entity.addEffect(newEffect);
     }
 
     private static void updateWithdrawEffects(HealthCapability health, LivingEntity entity) {
         var head = health.getComponent(HEAD);
         var state = head.getCondition(WITHDRAW);
         if (!head.abnormal(WITHDRAW)) return;
-        if (state.getValue() > 0.2f && !entity.hasEffect(ModEffects.CRAVING_EFFECT)) {
+        if (state.getValue() > 0.2f) {
             entity.addEffect(new MobEffectInstance(ModEffects.CRAVING_EFFECT, 100));
         }
-        if (state.getValue() > 0.3f && !entity.hasEffect(ModEffects.SWEATING_EFFECT)) {
+        if (state.getValue() > 0.3f) {
             entity.addEffect(new MobEffectInstance(ModEffects.SWEATING_EFFECT, 100));
         }
-        if (state.getValue() > 0.4f && !entity.hasEffect(MobEffects.CONFUSION)) {
+        if (state.getValue() > 0.4f) {
             entity.addEffect(new MobEffectInstance(MobEffects.CONFUSION, 200));
         }
     }
 
     private static void updateLivingTimeEffects(HealthCapability health, LivingEntity entity) {
-        if (entity.hasEffect(ModEffects.KEEP_LIVING_EFFECT)) return;
         int amp = (int) Math.sqrt((double) health.livingTick() / 1000);
         amp = Math.min(amp, 40);
         if (amp < 1) return;
@@ -90,7 +84,6 @@ public class BuffEventHandler {
     }
 
     private static void updateCureEffects(HealthCapability health, LivingEntity entity) {
-        if (entity.hasEffect(ModEffects.CURE_EFFECT)) return;
         if (health.vitality() < 0.999f) return;
         if (health.almostDead() < 0.2f) {
             entity.addEffect(new MobEffectInstance(ModEffects.CURE_EFFECT, 2400, 2));
@@ -105,29 +98,21 @@ public class BuffEventHandler {
     }
 
     private static void updateSymptomsEffects(HealthCapability health, LivingEntity entity) {
-        if (!entity.hasEffect(ModEffects.INTENSE_PAIN_EFFECT) && health.intensePain()) {
-            if (Mth.randomBetween(Utils.randomSource, 0.0f, 1.0f) < 0.007f) {
-                var newEffect = new MobEffectInstance(ModEffects.INTENSE_PAIN_EFFECT, 60);
-                entity.addEffect(newEffect);
-            }
+        if (health.intensePain() && Mth.randomBetween(Utils.randomSource, 0.0f, 1.0f) < 0.007f) {
+            var newEffect = new MobEffectInstance(ModEffects.INTENSE_PAIN_EFFECT, 60);
+            entity.addEffect(newEffect);
         }
-        if (!entity.hasEffect(ModEffects.PALE_SKIN) && health.getComponent(BLOOD).getConditionValue(BLOOD_LOSS) > 0.4f) {
+        if (health.getComponent(BLOOD).getConditionValue(BLOOD_LOSS) > 0.4f) {
             entity.addEffect(new MobEffectInstance(ModEffects.PALE_SKIN, 100));
         }
-        if (!entity.hasEffect(ModEffects.HARD_BREATH)) {
-            if (health.getComponent(BLOOD).getConditionValue(OXYGEN) > 0.2f || health.getComponent(TORSO).abnormal(RESPIRATORY_ARREST))
-                entity.addEffect(new MobEffectInstance(ModEffects.HARD_BREATH, 100));
-        }
-        if (!entity.hasEffect(MobEffects.BLINDNESS)) {
-            if (health.getComponent(BLOOD).getConditionValue(OXYGEN) > 0.5f)
-                entity.addEffect(new MobEffectInstance(MobEffects.DARKNESS, 100));
-        }
-        if (!entity.hasEffect(MobEffects.WEAKNESS)) {
-            var value = health.getComponent(BLOOD).getConditionValue(BLOOD_LOSS);
-            if (value > 0.6f)
-                entity.addEffect(new MobEffectInstance(MobEffects.WEAKNESS, 100,
-                        (int) ((value - 0.6f) / 0.2f)
-                ));
-        }
+        if (health.getComponent(BLOOD).getConditionValue(OXYGEN) > 0.2f || health.getComponent(TORSO).abnormal(RESPIRATORY_ARREST))
+            entity.addEffect(new MobEffectInstance(ModEffects.HARD_BREATH, 100));
+        if (health.getComponent(BLOOD).getConditionValue(OXYGEN) > 0.5f)
+            entity.addEffect(new MobEffectInstance(MobEffects.DARKNESS, 100));
+        var value = health.getComponent(BLOOD).getConditionValue(BLOOD_LOSS);
+        if (value > 0.6f)
+            entity.addEffect(new MobEffectInstance(MobEffects.WEAKNESS, 100,
+                    (int) ((value - 0.6f) / 0.2f)
+            ));
     }
 }
