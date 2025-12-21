8bceaa5870b040886bb021cc52d7e49ea0c61e1c
LAST-iMP <xiaoshibang@126.com>
2025-12-21 22:09:39 +0800

    init version 1.1.7.7

--- src/main/java/com/lastimp/dgh/source/register/ModEventBus.java
+++ src/main/java/com/lastimp/dgh/source/register/ModEventBus.java
@@ -2,14 +2,19 @@
 package com.lastimp.dgh.source.register;
 
 import com.lastimp.dgh.DontGetHurt;
+import com.lastimp.dgh.neoforge.Common;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.damagesource.DamageType;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.common.capabilities.RegisterCapabilitiesEvent;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.DataPackRegistryEvent;
+import vazkii.patchouli.api.PatchouliAPI;
 
 @Mod.EventBusSubscriber(modid = DontGetHurt.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ModEventBus {
@@ -17,5 +22,22 @@ public class ModEventBus {
     @SubscribeEvent
     public static void registerCapabilities(RegisterCapabilitiesEvent event) {
         ModCapabilities.register(event);
+    }
+
+    @SubscribeEvent
+    public static void onPlayerLogin(PlayerEvent.PlayerLoggedInEvent event) {
+        if (event.getEntity().level().isClientSide) return;
+
+        var player = event.getEntity();
+
+        var data = player.getPersistentData();
+        var key = "dgh_new_player";
+
+        if (!data.getBoolean(key)) {
+            player.getInventory().add(new ItemStack(ModItems.HEALTH_CARE_BAG.get()));
+            player.getInventory().add(new ItemStack(ModItems.BANDAGE.get(), 8));
+            player.getInventory().add(new ItemStack(ModItems.MORPHINE.get(), 2));
+            data.putBoolean(key, true);
+        }
     }
 }
