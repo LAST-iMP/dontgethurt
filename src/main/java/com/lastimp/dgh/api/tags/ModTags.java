bf409f8b5b183af244470cc0b73b6b0c90c2e900
LAST-iMP <xiaoshibang@126.com>
2025-12-29 13:34:47 +0800

    init version 1.2.1.7

--- src/main/java/com/lastimp/dgh/api/tags/ModTags.java
+++ src/main/java/com/lastimp/dgh/api/tags/ModTags.java
@@ -1,10 +1,13 @@
 package com.lastimp.dgh.api.tags;
 
 import com.lastimp.dgh.neoforge.Common;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class ModTags {
     public static final TagKey<Item> MEDICINE = ItemTags.create(Common.ResourceLocation("c", "medicine"));
@@ -17,4 +20,39 @@ public class ModTags {
 
     public static final TagKey<Item> OXYGEN_SUPPLIERS = ItemTags.create(Common.ResourceLocation("c", "oxygen_suppliers"));
     public static final TagKey<Item> AUTOPULSE = ItemTags.create(Common.ResourceLocation("c", "autopulse"));
+
+    private static final Set<TagKey<Item>> shearSet = new HashSet<>();
+    private static final Set<TagKey<Item>> healthScreenAvaItem = new HashSet<>();
+
+    static {
+        addShearTage(MEDICAL_TOOLS_SHEARS);
+
+        addHealthScreenAvaItem(ModTags.MEDICINE);
+        addHealthScreenAvaItem(ModTags.MEDICAL_TOOLS);
+        addHealthScreenAvaItem(ModTags.MEDICAL_TOOLS_BAGS);
+    }
+
+    public static void addShearTage(TagKey<Item> key) {
+        shearSet.add(key);
+    }
+
+    public static void addHealthScreenAvaItem(TagKey<Item> key) {
+        healthScreenAvaItem.add(key);
+    }
+
+    public static boolean isShears(ItemStack stack) {
+        for (var key : shearSet) {
+            if (stack.is(key))
+                return true;
+        }
+        return false;
+    }
+
+    public static boolean isHealthScreenAvaItem(ItemStack stack) {
+        for (var key : healthScreenAvaItem) {
+            if (stack.is(key))
+                return true;
+        }
+        return false;
+    }
 }
--- src/main/java/com/lastimp/dgh/data/ModItemTagsProvider.java
+++ src/main/java/com/lastimp/dgh/data/ModItemTagsProvider.java
@@ -2,6 +2,7 @@
 package com.lastimp.dgh.data;
 
 import com.lastimp.dgh.api.tags.ModTags;
+import com.lastimp.dgh.neoforge.Common;
 import com.lastimp.dgh.source.register.ModItems;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.registries.Registries;
@@ -127,5 +128,39 @@ public class ModItemTagsProvider extends TagsProvider<Item> {
                 .add(ModItems.OXYGEN_MASK.getKey());
         this.tag(ModTags.AUTOPULSE)
                 .add(ModItems.AUTOPULSE.getKey());
+
+        //tfc
+        this.tag(ModTags.MEDICAL_TOOLS)
+                .addOptional(Common.ResourceLocation("tfc", "metal/shears/bismuth_bronze"))
+                .addOptional(Common.ResourceLocation("tfc", "metal/shears/black_bronze"))
+                .addOptional(Common.ResourceLocation("tfc", "metal/shears/bronze"))
+                .addOptional(Common.ResourceLocation("tfc", "metal/shears/copper"))
+                .addOptional(Common.ResourceLocation("tfc", "metal/shears/wrought_iron"))
+                .addOptional(Common.ResourceLocation("tfc", "metal/shears/steel"))
+                .addOptional(Common.ResourceLocation("tfc", "metal/shears/black_steel"))
+                .addOptional(Common.ResourceLocation("tfc", "metal/shears/blue_steel"))
+                .addOptional(Common.ResourceLocation("tfc", "metal/shears/red_steel"));
+
+        this.tag(ModTags.MEDICAL_TOOLS_BASIC)
+                .addOptional(Common.ResourceLocation("tfc", "metal/shears/bismuth_bronze"))
+                .addOptional(Common.ResourceLocation("tfc", "metal/shears/black_bronze"))
+                .addOptional(Common.ResourceLocation("tfc", "metal/shears/bronze"))
+                .addOptional(Common.ResourceLocation("tfc", "metal/shears/copper"))
+                .addOptional(Common.ResourceLocation("tfc", "metal/shears/wrought_iron"))
+                .addOptional(Common.ResourceLocation("tfc", "metal/shears/steel"))
+                .addOptional(Common.ResourceLocation("tfc", "metal/shears/black_steel"))
+                .addOptional(Common.ResourceLocation("tfc", "metal/shears/blue_steel"))
+                .addOptional(Common.ResourceLocation("tfc", "metal/shears/red_steel"));
+
+        this.tag(ModTags.MEDICAL_TOOLS_SHEARS)
+                .addOptional(Common.ResourceLocation("tfc", "metal/shears/bismuth_bronze"))
+                .addOptional(Common.ResourceLocation("tfc", "metal/shears/black_bronze"))
+                .addOptional(Common.ResourceLocation("tfc", "metal/shears/bronze"))
+                .addOptional(Common.ResourceLocation("tfc", "metal/shears/copper"))
+                .addOptional(Common.ResourceLocation("tfc", "metal/shears/wrought_iron"))
+                .addOptional(Common.ResourceLocation("tfc", "metal/shears/steel"))
+                .addOptional(Common.ResourceLocation("tfc", "metal/shears/black_steel"))
+                .addOptional(Common.ResourceLocation("tfc", "metal/shears/blue_steel"))
+                .addOptional(Common.ResourceLocation("tfc", "metal/shears/red_steel"));
     }
 }
