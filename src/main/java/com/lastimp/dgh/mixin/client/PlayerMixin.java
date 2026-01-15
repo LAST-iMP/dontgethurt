136d8f94e3201f1189eed1f53ee6252c08d801bf
LAST-iMP <xiaoshibang@126.com>
2026-01-16 02:44:10 +0800

    init version 1.2.8.5

---
+++ src/main/java/com/lastimp/dgh/mixin/client/PlayerMixin.java
@@ -0,0 +1,42 @@
+package com.lastimp.dgh.mixin.client;
+
+import com.lastimp.dgh.source.core.capability.HealthCapability;
+import com.mojang.authlib.GameProfile;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Items;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Player.class)
+public class PlayerMixin {
+    @Inject(at = @At("HEAD"), method = "updatePlayerPose", cancellable = true)
+    private void incapacitatedUpdatePlayerPose(CallbackInfo ci){
+        Player player = (Player)(Object)this;
+        if (HealthCapability.has(player) && HealthCapability.isDying(player)) {
+            if (player.getVehicle() != null) return;
+            ci.cancel();
+        }
+    }
+
+    @Inject(method = "getName", at = @At("HEAD"), cancellable = true)
+    private void changeName(CallbackInfoReturnable<Component> cir) {
+        Player player = (Player)(Object)this;
+        var stack = player.getSlot(103).get();
+        if (stack.isEmpty()) return;
+        if (!stack.is(Items.PLAYER_HEAD)) return;
+
+        var tag = stack.get(DataComponents.PROFILE);
+        if (tag == null) return;
+
+        GameProfile profile = tag.gameProfile();
+        if (profile != null) {
+            cir.setReturnValue(Component.nullToEmpty(profile.getName()));
+        }
+    }
+}
+
---
+++ src/main/java/com/lastimp/dgh/mixin/client/PlayerSkinMixin.java
@@ -0,0 +1,34 @@
+package com.lastimp.dgh.mixin.client;
+
+import com.mojang.authlib.GameProfile;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.PlayerInfo;
+import net.minecraft.client.player.AbstractClientPlayer;
+import net.minecraft.client.resources.PlayerSkin;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.nbt.NbtUtils;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Items;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(AbstractClientPlayer.class)
+public class PlayerSkinMixin {
+    @Inject(method = "getSkin", at = @At("RETURN"), cancellable = true)
+    private void useOtherSkin(CallbackInfoReturnable<PlayerSkin> cir) {
+        AbstractClientPlayer player = (AbstractClientPlayer) (Object) this;
+        var stack = player.getSlot(103).get();
+        if (stack.isEmpty()) return;
+        if (!stack.is(Items.PLAYER_HEAD)) return;
+
+        var tag = stack.get(DataComponents.PROFILE);
+        if (tag == null) return;
+
+        GameProfile profile = tag.gameProfile();
+        if (profile != null) {
+            cir.setReturnValue(Minecraft.getInstance().getConnection().getPlayerInfo(profile.getId()).getSkin());
+        }
+    }
+}
