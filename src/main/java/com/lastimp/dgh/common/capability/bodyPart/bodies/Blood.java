5b631567d66da0778157e2fd227023ff691cba83
LAST-iMP <xiaoshibang@126.com>
2026-02-07 11:12:29 +0800

    init version 1.3.1.1

--- src/main/java/com/lastimp/dgh/common/capability/bodyPart/bodies/Blood.java
+++ src/main/java/com/lastimp/dgh/common/capability/bodyPart/bodies/Blood.java
@@ -197,6 +197,10 @@ public class Blood extends AbstractBody {
         this.sepsis += gangrene * ConditionAccessor.get(SEPSIS).healingSpeed() / 0.15f;
         this.sepsis += foreign_object * ConditionAccessor.get(SEPSIS).healingSpeed() / 0.15f;
         health.getComponent(BLOOD).injury(SEPSIS, this.sepsis * Utils.DELTA);
+
+        if (this.abnormal(ANTIBIOTICS) && this.getConditionValue(OXYGEN) < 0.3f && this.getConditionValue(BLOOD_PRESSURE) > 0.7f) {
+            this.healing(SEPSIS, -ConditionAccessor.get(SEPSIS).healingSpeed() * Utils.DELTA * 2);
+        }
     }
 
     private void handleCombatStimulant(HealthCapability health, LivingEntity entity) {
