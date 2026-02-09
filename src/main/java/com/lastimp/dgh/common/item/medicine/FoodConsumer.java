b9e3555d3010aa6251b418ac3a566bcc63cc638c
LAST-iMP <xiaoshibang@126.com>
2026-02-09 09:43:55 +0800

    init version 1.3.1.2

--- src/main/java/com/lastimp/dgh/common/item/medicine/FoodConsumer.java
+++ src/main/java/com/lastimp/dgh/common/item/medicine/FoodConsumer.java
@@ -20,8 +20,7 @@ public class FoodConsumer extends AbstractDirectHealItems {
 
     @Override
     public boolean heal(@NotNull LivingEntity source, @NotNull LivingEntity entity) {
-        if (!(entity instanceof Player player)) return false;
-        player.addEffect(new MobEffectInstance(ModEffects.FOOD_CONSUMER_EFFECT, 20 * 120));
+        entity.addEffect(new MobEffectInstance(ModEffects.FOOD_CONSUMER_EFFECT, 20 * 120));
         return true;
     }
 
